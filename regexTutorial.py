485df5cc42e09ba3c092430775056f8b281a1b83
SahilB2k <jadhavssahil@gmail.com>
2025-06-03 02:12:56 +0530

    exercise questions done

--- regexTutorial.py
+++ regexTutorial.py
@@ -2,26 +2,44 @@
 
 import re
 
-chat1='codebasics:you ask  lot of questions 12345678912, abc@xyz.com'
-chat2='codebasics:here it is : (123)-567-8912, abc@xyz.com'
-chat3='codebasics:yes , phone: 1235678912: abc@xyz.com'
-chat4='abc_56@vxy.com'
-chat5='hello , i have my issue with my order # 42134567'
-chat6='hello , i have my issue with my order number 42134567'
-chat7='My age is 66'
-chat8='hello i was born elon musk and my age 34'
+# chat1='codebasics:you ask  lot of questions 12345678912, abc@xyz.com'
+# chat2='codebasics:here it is : (123)-567-8912, abc@xyz.com'
+# chat3='codebasics:yes , phone: 1235678912: abc@xyz.com'
+# chat4='abc_56@vxy.com'
+# chat5='hello , i have my issue with my order # 42134567'
+# chat6='hello , i have my issue with my order number 42134567'
+# chat7='My age is 66'
+# chat8='hello i was born elon musk and my age 34'
 
 
-number=r'\d{10}' #for extracting numbers
-mails=r'[a-z0-9A-Z_]*@[a-z]*\.[a-z]*' #for extracting emails
-order_number=r'order[^\d]*(\d*)' #fr order number
-age=r'age[^\d]*(\d+)'  # Matches 'age' followed by any non-digit chars, then captures digits
-name=r'born(.*)'
+# number=r'\d{10}' #for extracting numbers
+# mails=r'[a-z0-9A-Z_]*@[a-z]*\.[a-z]*' #for extracting emails
+# order_number=r'order[^\d]*(\d*)' #fr order number
+# age=r'age[^\d]*(\d+)'  # Matches 'age' followed by any non-digit chars, then captures digits
+# name=r'born(.*)'
 
 
-matches=re.findall(name,chat8)
-print(matches)
+# matches=re.findall(name,chat8)
+# print(matches)
 
+
+text='''
+Look for data to help you address the question. Governments are good
+sources because data from public research is often freely available. Good
+places to start include http://www.data.gov/, and http://www.science.
+gov/, and in the United Kingdom, http://data.gov.uk/.
+Two of my favorite data sets are the General Social Survey at http://www3.norc.org/gss+website/, 
+and the European Social Survey at http://www.europeansocialsurvey.org/.
+'''
+
+# url=r'http[^\d]*[a-z]*\n'
+# urls=re.findall(url,text)
+# print(urls)
+
+transactions = "Tony gave two $ to Peter, Bruce gave 500 € to Steve"
+money=r'[a-z]*[^\d]'
+moneys=re.findall(money,transactions)
+print(moneys)
 
 
 
--- tutorialSpacy.py
+++ tutorialSpacy.py
@@ -32,11 +32,48 @@ text=' '.join(text) #converting array to a single line
 # for token in doc:
 #     if token.like_email:
 #         email.append(token.text)
-# print(email)                         #extracting only the email
+# print(email)                       #extracting only the email
 
-nlp2=spacy.blank('hi')
-doc2=nlp("क्ऐसे हो भैया,मेरे 500 उपय्य उधर दे दो !!")
+
+
+# nlp2=spacy.blank('hi')
+# doc2=nlp("क्ऐसे हो भैया,मेरे 500 उपय्य उधर दे दो !!")
+# for token in doc2:
+#     print(token,token.is_currency,token.like_num)
+
+# from spacy.symbols import ORTH
+
+# doc=nlp("gimme double cheese extra large healthy pizza")
+# tokens=[token.text for token in doc] #priting tokens directly in array
+# print(tokens)  
+
+
+# nlp.tokenizer.add_special_case("gimme",[  #using ORTH lib to convert special words to their root/base words
+#     {ORTH:"gim"},
+#     {ORTH:"me"}
+# ])
+
+# doc2=nlp("gimme double cheese extra large healthy pizza")
+
+# tokens=[token.text for token in doc2]
+# print(tokens) #printing all the base words from the text
+
+
+text='''
+Look for data to help you address the question. Governments are good
+sources because data from public research is often freely available. Good
+places to start include http://www.data.gov/, and http://www.science.
+gov/, and in the United Kingdom, http://data.gov.uk/.
+Two of my favorite data sets are the General Social Survey at http://www3.norc.org/gss+website/, 
+and the European Social Survey at http://www.europeansocialsurvey.org/.
+'''
+
+doc=nlp(text)
+data_website=[token.text for token in doc if token.like_url]
+print(data_website)
+
+transactions = "Tony gave two $ to Peter, Bruce gave 500 € to Steve"
+doc2=nlp(transactions)
 for token in doc2:
-    print(token,token.is_currency,token.like_num)
-
-
+    if token.like_num and doc2[token.i+1].is_currency:
+        print(token,doc[token.i+1].text)
